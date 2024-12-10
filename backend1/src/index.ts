7ae0e85951ff026e09058cedd508250216216112
Parth0124 <abhangparth@gmail.com>
2024-12-11 00:27:10 +0530

    created a basic web socket server

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,13 +1,15 @@
 import { WebSocketServer } from 'ws';
+import { GameManager } from './GameManager';
 
 const wss = new WebSocketServer({port:4000});
 
+const gameManager = new GameManager()
+
 wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+    gameManager.addUser(ws)
 
-    ws.on('message', function message(data){
-        console.log('received: %s', data);
-    });
-
+    ws.on('disconnect', () => gameManager.removeUser(ws))
+    
     ws.send('something')
-}) 
+
+});
